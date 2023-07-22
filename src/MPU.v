f8ddede41e704473d2f8234d95829f485f6e331c
yannickreiss <yannick.reiss@protonmail.ch>
2023-07-22 16:23:26 +0200

    Implement HALT automatism and phase_counter

--- src/MPU.v
+++ src/MPU.v
@@ -8,41 +8,69 @@ module tt_um_yannickreiss_semaphore (input wire [7:0] ui_in,    // Dedicated inp
                                      input wire ena,
                                      input wire clk,
                                      input wire rst_n);
-// Signals
-wire mutex;
-reg [7:0] bus_reg;
-reg [7:0] fcfs[7:0];
 
-assign mutex = uo_out[0] || uo_out[1] || uo_out[2] || uo_out[3] || uo_out[4] || uo_out[5] || uo_out[6] || uo_out[7];
+// internal memory
+reg [7:0] register [7:0]; // registers
+reg [11:0] program_counter; // Count up to 
+reg [2:0] phase_counter; // current phase
+reg [2:0] alt_phase; // store phase here, while HALT
+reg [7:0] alu_in1; // first alu input
+reg [7:0] alu_in2; // second alu input
+reg [7:0] instruction; // store current instruction
+reg HALT_s;
 
-assign uo_out = bus_reg & mutex;
+// in- and output signals
+wire HALT;
+assign HALT = ui_in[7];
 
-// reset
+// Reset
 always @(negedge rst_n) begin
-    bus_reg = 8'b0;
-    fcfs[7] = 8'b0;
-    fcfs[6] = 8'b0;
-    fcfs[5] = 8'b0;
-    fcfs[4] = 8'b0;
-    fcfs[3] = 8'b0;
-    fcfs[2] = 8'b0;
-    fcfs[1] = 8'b0;
-    fcfs[0] = 8'b0;
+    register[7] = 8'b0;
+    register[6] = 8'b0;
+    register[5] = 8'b0;
+    register[4] = 8'b0;
+    register[3] = 8'b0;
+    register[2] = 8'b0;
+    register[1] = 8'b0;
+    register[0] = 8'b0;
+
+    program_counter = 12'b0;
+
+    alt_phase = 3'b0;
+    phase_counter = 3'b0;
+    HALT_s = 1'b0;
 end
 
-// clk
+// Phase counter
 always @(posedge clk) begin
-    if (mutex == 1'b0) begin
-        // LOCK
-        bus_reg = fcfs[7];
-        fcfs[7] = fcfs[6];
-        fcfs[6] = fcfs[5];
-        fcfs[5] = fcfs[4];
-        fcfs[4] = fcfs[3];
-        fcfs[3] = fcfs[2];
-        fcfs[2] = fcfs[1];
-        fcfs[1] = fcfs[0];
-    end
+    case (phase_counter)
+        3'b000: phase_counter = 3'b001; // IF -> Dec
+        3'b001: phase_counter = 3'b001 ^ instruction[7:5]; // Dec -> Opcode (010 xor)
+        3'b010: phase_counter = 3'b011; // operand 3 -> op2
+        3'b011: phase_counter = 3'b100; // operand 2 -> op1
+        3'b100: phase_counter = 3'b101; // operand 1 -> exec
+        3'b101: phase_counter = 3'b110; // Exec -> Write back
+        3'b110: phase_counter = 3'b000; // Write back -> IF
+        default: phase_counter = 3'b111;
+    endcase
 end
+
+// Block the MPU
+always @(posedge HALT_s) begin
+    alt_phase = phase_counter;
+    phase_counter = 3'b111;
+end
+
+// Release the MPU
+always @(negedge HALT) begin
+    HALT_s = 1'b0;
+    phase_counter = alt_phase;
+end
+
+// program counter
+
+
+// ALU
+
 
 endmodule
